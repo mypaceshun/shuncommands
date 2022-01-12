61d903fb7b31fdc57cfe00b9eaf648edf04952cd
KAWAI Shun <shun@osstech.co.jp>
2022-01-12 18:00:57 +0900

    black python-cov の導入

--- tests/test_pdftool.py
+++ tests/test_pdftool.py
@@ -3,35 +3,35 @@ from click.testing import CliRunner
 from shuncommands import pdftool
 
 
-class TestPdfTool():
+class TestPdfTool:
     def test_run_help(self):
         runner = CliRunner()
-        res = runner.invoke(pdftool.ctx, ['--help'])
+        res = runner.invoke(pdftool.ctx, ["--help"])
         assert res.exit_code == 0
 
-        res = runner.invoke(pdftool.ctx, ['-h'])
+        res = runner.invoke(pdftool.ctx, ["-h"])
         assert res.exit_code == 0
 
     def test_run_version(self):
         runner = CliRunner()
-        res = runner.invoke(pdftool.ctx, ['--version'])
+        res = runner.invoke(pdftool.ctx, ["--version"])
         assert res.exit_code == 0
 
-        res = runner.invoke(pdftool.ctx, ['-v'])
+        res = runner.invoke(pdftool.ctx, ["-v"])
         assert res.exit_code == 0
 
     def test_run_unlock(self):
         runner = CliRunner()
-        res = runner.invoke(pdftool.ctx, ['unlock', '--help'])
+        res = runner.invoke(pdftool.ctx, ["unlock", "--help"])
         assert res.exit_code == 0
 
-        res = runner.invoke(pdftool.ctx, ['unlock', '-h'])
+        res = runner.invoke(pdftool.ctx, ["unlock", "-h"])
         assert res.exit_code == 0
 
     def test_run_text(self):
         runner = CliRunner()
-        res = runner.invoke(pdftool.ctx, ['text', '--help'])
+        res = runner.invoke(pdftool.ctx, ["text", "--help"])
         assert res.exit_code == 0
 
-        res = runner.invoke(pdftool.ctx, ['text', '-h'])
+        res = runner.invoke(pdftool.ctx, ["text", "-h"])
         assert res.exit_code == 0
